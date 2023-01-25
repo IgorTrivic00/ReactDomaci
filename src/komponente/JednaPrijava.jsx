2d3a9e8cc60691a2ffd4b405ca84ff4f817650c4
IgorTrivic00 <trivicigor00@gmail.com>
2023-01-25 20:03:55 +0100

    Definisana pojedinacna prijava

--- src/komponente/JednaPrijava.jsx
+++ src/komponente/JednaPrijava.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
-function JednaPrijava() {
+function JednaPrijava({prijava,zavrsi,obrisi}) {
   return (
-    <div>
-      
+    <div className="col">
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{prijava.ime}</h5>
+                <p className="card-text">{prijava.pozicija}</p>
+                <button className="btn btn-success card-link" onClick={()=>zavrsi(prijava.id)}>Primi kandidata</button>
+                <button className="btn btn-danger card-link" onClick={()=>obrisi(prijava.id)}>Odbij</button>
+            </div>
+            <div className="card-footer text-muted">
+                Status: {prijava.status}
+            </div>
+        </div>
     </div>
-  )
+);
 }
 
 export default JednaPrijava
